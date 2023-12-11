65e22a45447f4732bb3194d525de5d9231856248
unknown <sujalverdhen1234@gmail.com>
2023-12-11 21:22:54 +0530

    make responsive

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,34 +1,41 @@
-import React from "react"
-import "./container.css"
-import Maps from "../../assets/Maps.png"
-import Img from "../../assets/Rectangle 285.png"
-import Camera from "../../assets/camera.png"
+import React from "react";
+import "./container.css";
+import Maps from "../../assets/Maps.png";
+import Img from "../../assets/Rectangle 285.png";
+import Camera from "../../assets/camera.png";
 
-function container(){
-    
-return (<div className="container">
-<p>Complete Detail info</p>
-<div className="inside">
-    <div style={{paddingLeft:10}} className="location">
-        <img src={Maps}/>
-        <div style={{paddingLeft:10}}>
-
-        <p style={{marginBottom:0}} className="yourcountry">Your Country</p>
-        <p style={{marginTop:0}}>Indonesia</p>
+function container() {
+  return (
+    <div className="container">
+      <p>Complete Detail info</p>
+      <div className="inside">
+        <div style={{ paddingLeft: 10 }} className="location">
+          <img src={Maps} />
+          <div style={{ paddingLeft: 10 }}>
+            <p style={{ marginBottom: 0 }} className="yourcountry">
+              Your Country
+            </p>
+            <p style={{ marginTop: 0 }}>Indonesia</p>
+          </div>
         </div>
+        <p>Verify your documents</p>
+        <p className="subhead">
+          Upload your ID and relevant Educational certifications
+        </p>
+        {/* <p>National ID Card</p> */}
+        <div className="links">
+         <input type="text" placeholder="National ID Card"></input> <img src={Img} />
+        </div>
+        <div className="links">
+        <input type="text" placeholder="Certificate of being a specialist"></input>   <img src={Camera} />
+        </div>
+        <div className="links">
+        <input type="text" placeholder="Add Linkedin profile link"></input> 
+        </div>
+        <button className="Button">Save</button>
+      </div>
     </div>
-    <p>
-        Verify your documents
-    </p>
-    <p className="subhead">Upload your ID and relevant Educational certifications</p>
-
-    <div className="links"><p>National ID Card</p> <img src={Img}/></div>
-    <div className="links"><p>Certificate of being a specialist</p> <img src={Camera}/></div>
-    <div className="links"><p>Add Linkedin profile link</p> </div>
-   <button className="Button">Save</button>
-   
-</div>
-</div>)
+  );
 }
 
-export default container
+export default container;
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,17 +1,25 @@
-import React from "react"
+import React,{useState} from "react"
 import logo from "../../assets/Group 889.svg"
 import Navig from "../../assets/Navigation.png"
 import Avatar from "../../assets/Avatar.png"
 import Bell from "../../assets/bell.png"
 import Beel from "../../assets/beel2.png"
 import "./nav.css"
-function nav(){
+function Nav(){
+   const [ison,setison]=useState(false);
     return (<div className="nav">
      <img src={logo}/>
      <div className="mid">
      <img src={Navig}/>
         <p>Become a specialist</p>
      </div>
+     <button className="sidelogin" onClick={()=>{ setison(prevvalue=>!prevvalue)
+     }}>Click</button>
+     <ul style={{display:ison?"flex":"none",flexDirection:"column",justifyContent:"center",alignItems:"center"}} className="dialog">
+      <li >Profile</li>
+      <li>Setting</li>
+      <li className="logout">Logout</li>
+     </ul>
      <div className="last">
      <div className="login">
      <img style={{marginRight:10}} className="avatar" src={Avatar}/>
@@ -23,4 +31,4 @@ function nav(){
     </div>)
 }
 
-export default nav;
+export default Nav;
